359e8183a630c011c003374451eb281263ab5dab
bluckau <bluckau@users.noreply.github.com>
2018-07-27 10:00:54 -0600

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,5 @@
+# Dashboard https://portal.routerlimits.com/user/dashboard
+# https://portal.routerlimits.com/user/groups
+# https://portal.routerlimits.com/user/limits
+# https://portal.routerlimits.com/user/history
+# https://portal.routerlimits.com/user/settings
